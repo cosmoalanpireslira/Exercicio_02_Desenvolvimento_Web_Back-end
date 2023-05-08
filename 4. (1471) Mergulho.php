524e229ad820c922856a093aa833288d42ef2075
Cosmo Alan Pires Lira <105260581+cosmoalanpireslira@users.noreply.github.com>
2023-05-08 03:39:28 -0300

    Update 4. (1471) Mergulho.php

--- 4. (1471) Mergulho.php
+++ 4. (1471) Mergulho.php
@@ -1,47 +1,46 @@
 <?php
 
-while (true) {
-  $entrada = explode(" ", fgets(STDIN));    // Lê o número de pessoas que foram à festa e o número de pessoas que faltaram
+while (true) {     // Lê o número de mergulhadores e o número de mergulhadores que retornaram
+  $entrada = explode(" ", fgets(STDIN));
   $n = intval($entrada[0]);
   $r = intval($entrada[1]);
 
-  $presenca = array_fill(1, $n, 0);   // Cria um array com o número de presenças e inicializa com zero
+  $mergulhadores = array_fill(1, $n, 0);    // Cria um array com o número de mergulhadores e inicializa com zero
 
-  $pessoas = explode(" ", fgets(STDIN));     // Lê os números das pessoas que foram à festa
-  foreach ($pessoas as $pessoa) {
-    $presenca[intval($pessoa)] = 1;
+  $retornaram = explode(" ", fgets(STDIN));    // Lê os números dos mergulhadores que retornaram
+  foreach ($retornaram as $mergulhador) {
+    $mergulhadores[intval($mergulhador)] = 1;
   }
 
-  $faltantes = "";
-  
-  for ($i = 1; $i <= $n; $i++) {     // Verifica quais pessoas faltaram à festa
-    if ($presenca[$i] == 0) {
-      $faltantes .= $i . " ";
+  $nao_retornaram = "";   // Verifica quais mergulhadores não retornaram
+  for ($i = 1; $i <= $n; $i++) {
+    if ($mergulhadores[$i] == 0) {
+      $nao_retornaram .= $i . " ";
     }
   }
 
-  if ($faltantes == "") {   // Imprime os números das pessoas que faltaram ou a mensagem "sim" se todas foram à festa
-    echo "sim\n";
+  if ($nao_retornaram == "") {    // Imprime os números dos mergulhadores que não retornaram ou a mensagem "todos voltaram" se todos retornaram
+    echo "Todos voltaram!\n";
   } else {
-    echo trim($faltantes) . "\n";
+    echo trim($nao_retornaram) . "\n";
   }
 }
 
 /*
 - O loop while começa na linha 3 e será executado indefinidamente (até que seja interrompido por um comando break).
-- A linha 4 lê os valores de n e r (o número de pessoas que foram à festa e o número de pessoas que faltaram)
+- A linha 5 e 6 lê os valores de n e r (o número de mergulhadores e o número de mergulhadores que retornaram) 
 utilizando a função fgets e a função explode para separar os valores em um array.
-- A linha 8 cria um array chamado $presenca com o número de presenças e inicializa todos os elementos com zero utilizando a função array_fill.
-- A linha 17 lê os números das pessoas que foram à festa utilizando a função fgets e o loop foreach 
-para percorrer o array de pessoas e marcar as presenças no array $presenca.
-- A linha 23 verificam quais pessoas faltaram à festa, utilizando um loop for e um condicional if.
-As pessoas que faltaram são adicionadas à string $faltantes.
-- A linha 26 imprime a mensagem "sim" se todas as pessoas foram à festa, ou os números das pessoas que faltaram, 
-utilizando a função echo e a concatenação de strings. A função trim é utilizada para remover o espaço em branco no final da string $faltantes.
+- A linha 8 cria um array chamado $mergulhadores com o número de mergulhadores e inicializa todos os elementos com zero utilizando a função array_fill.
+- A linhaa 10 lê os números dos mergulhadores que retornaram utilizando a função fgets e o loop foreach 
+para percorrer o array de mergulhadores e marcar os que retornaram no array $mergulhadores.
+- As linhas 15 a 18 verificam quais mergulhadores não retornaram, utilizando um loop for e um condicional if.
+Os mergulhadores que não retornaram são adicionados à string $nao_retornaram.
+- A linha 23 imprime a mensagem "Todos voltaram!" se todos os mergulhadores retornaram, ou os números dos mergulhadores que não retornaram, 
+utilizando a função echo e a concatenação de strings. A função trim é utilizada para remover o espaço em branco no final da string $nao_retornaram.
 - O loop retorna à linha 3 para ler os valores de n e r do próximo caso de teste.
 
 Obs.: a solução utiliza um loop while infinito para ler vários casos de teste. 
-A leitura de cada caso de teste é feita utilizando as funções fgets e explode. 
-Em seguida, é criado um array para armazenar as presenças e marcadas as presenças utilizando um loop foreach. 
-Finalmente, é verificado quais pessoas faltaram e o resultado é impresso utilizando a função echo e a concatenação de strings.
+A leitura de cada caso de teste é feita utilizando as funções fgets e explode.
+Em seguida, é criado um array para armazenar os mergulhadores e marcados os que retornaram utilizando um loop foreach. Finalmente, é verificado
 */
+
