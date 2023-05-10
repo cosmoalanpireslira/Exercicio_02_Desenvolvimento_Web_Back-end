0175f14cd9564b204c4fee487d50d2691ad4a3ca
Cosmo Alan Pires Lira <105260581+cosmoalanpireslira@users.noreply.github.com>
2023-05-10 17:44:25 -0300

    Update 2.(1171) Frequência de Números.php
    - A segunda linha lê a quantidade de números que serão lidos.
    - A terceira linha inicializa um array vazio para armazenar a frequência de cada número.
    - Na quarta linha o loop for começa aqui. Ele vai iterar num_numeros vezes, ou seja, uma vez para cada número que será lido.
    - Dentro do loop, a quinta linha lê o número atual.
    - A quinta linha verifica se o número ainda não está no array de frequência. Se não estiver, adiciona o número ao array com valor 1.
    - Caso contrário, a decima linha incrementa o valor da frequência para o número atual.
    - A nona linha ordena o array de frequência em ordem crescente de chaves (ou seja, em ordem crescente de números).
    - O loop foreach começa na linha 14. Ele itera sobre cada número e sua frequência no array de frequência.
    - Dentro do loop, a linha 15 imprime a frequência do número.

--- 2.(1171) Frequência de Números.php
+++ 2.(1171) Frequência de Números.php
@@ -15,17 +15,4 @@ foreach ($frequencia as $numero => $freq) {  // para cada item (na variável $fr
   echo "$numero aparece $freq vez(es)\n";   // Imprime a frequência de cada número
 }
 
-/*
-Explicação do código:
-- A primeira linha é o início do código.
-- A segunda linha lê a quantidade de números que serão lidos.
-- A terceira linha inicializa um array vazio para armazenar a frequência de cada número.
-- Na quarta linha o loop for começa aqui. Ele vai iterar num_numeros vezes, ou seja, uma vez para cada número que será lido.
-- Dentro do loop, a quinta linha lê o número atual.
-- A quinta linha verifica se o número ainda não está no array de frequência. Se não estiver, adiciona o número ao array com valor 1.
-- Caso contrário, a decima linha incrementa o valor da frequência para o número atual.
-- A nona linha ordena o array de frequência em ordem crescente de chaves (ou seja, em ordem crescente de números).
-- O loop foreach começa na linha 14. Ele itera sobre cada número e sua frequência no array de frequência.
-- Dentro do loop, a linha 15 imprime a frequência do número.
-- O programa termina.
-*/
+
