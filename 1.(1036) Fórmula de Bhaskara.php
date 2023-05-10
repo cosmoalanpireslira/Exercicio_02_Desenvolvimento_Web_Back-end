cb05af8de466ee199a633db35bd1951a19b4a4e7
Cosmo Alan Pires Lira <105260581+cosmoalanpireslira@users.noreply.github.com>
2023-05-10 17:43:34 -0300

    Update 1.(1036) Fórmula de Bhaskara.php
    A linha 2 lê os coeficientes a, b e c da equação de segundo grau. A função fgets(STDIN) lê uma linha da entrada padrão (no caso, o teclado)
    A linha 3 a função sscanf extrai os três valores da linha lida. O formato %f indica que os valores são do tipo float.
    A linha 4 calcula o delta da equação, utilizando a fórmula delta = b^2 - 4ac.
    A linha 6 verifica se a equação possui raízes reais. Se a for igual a zero ou delta for menor que zero, a equação não possui raízes reais e o programa imprime "Impossivel calcular".
    Caso contrário, a linha 10 e a linha 11 calculam as duas raízes da equação, utilizando a fórmula x = (-b ± sqrt(delta)) / 2a.
    As linhas 12 e 13 imprimem as raízes com 5 casas decimais utilizando a função printf, que permite formatar a saída. A string "%.5f" indica que o valor a ser impresso é um float com 5 casas decimais.
    O programa termina.
    Obs.: a solução utiliza a fórmula de Bhaskara para encontrar as raízes da equação de segundo grau. 
    A verificação se a equação possui raízes reais é necessária porque a raiz quadrada de um número negativo não é um número real.

--- 1.(1036) Fórmula de Bhaskara.php
+++ 1.(1036) Fórmula de Bhaskara.php
@@ -13,14 +13,5 @@ else {
   printf("R2 = %.5f\n", $x2);         // Imprime a raiz x2 com 5 casas decimais nesse caso a segunda raiz 
 }
 
-/*
-A linha 2 lê os coeficientes a, b e c da equação de segundo grau. A função fgets(STDIN) lê uma linha da entrada padrão (no caso, o teclado)
-A linha 3 a função sscanf extrai os três valores da linha lida. O formato %f indica que os valores são do tipo float.
-A linha 4 calcula o delta da equação, utilizando a fórmula delta = b^2 - 4ac.
-A linha 6 verifica se a equação possui raízes reais. Se a for igual a zero ou delta for menor que zero, a equação não possui raízes reais e o programa imprime "Impossivel calcular".
-Caso contrário, a linha 10 e a linha 11 calculam as duas raízes da equação, utilizando a fórmula x = (-b ± sqrt(delta)) / 2a.
-As linhas 12 e 13 imprimem as raízes com 5 casas decimais utilizando a função printf, que permite formatar a saída. A string "%.5f" indica que o valor a ser impresso é um float com 5 casas decimais.
-O programa termina.
-Obs.: a solução utiliza a fórmula de Bhaskara para encontrar as raízes da equação de segundo grau. 
-A verificação se a equação possui raízes reais é necessária porque a raiz quadrada de um número negativo não é um número real.
-*/
+
+
